9d2b5a38e276e794dbe44c55f8232b960f71a58f
choyoonme <yoonme@gmail.com>
2022-05-31 13:21:04 -0700

    add employee name and roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,35 @@
 INSERT INTO employees (id, first_name, last_name, roles_id, manager_id)
-VALUES (1,'bob', 'dylan', 1, 1),
-(2, 'paul', 'mccartney', 1, 2);
+VALUES (1,'Claude', 'Monet', 1, 1),
+(2, 'Paul', 'Cezanne', 5, 2),
+(3, 'Pierre-Auguste', 'Renoir', 6, 4),
+(4, 'Edouard', 'Manet', 3, 2),
+(5, 'Paul', 'Gaughin', 7, 4),
+(6, 'Henri', 'Matisse', 2, 1),
+(7, 'Georges', 'Seurat', 4, 1),
+(8, 'Henri', 'Rousseau', 7, 4),
+(9, 'Georges', 'Braque', 6, 4),
+(10, 'Eugene', 'Delacroix', 5, 2);
+
+
+
+
 
 
 INSERT INTO departments (id, department_name)
-VALUES (1, 'mailroom'),
-(2, 'janitor closet');
+VALUES (1, 'Administrative'),
+(2, 'Technical Services'),
+(3, 'Public Services'),
+(4,'Support Staff');
+
 
 INSERT INTO roles (id, title_name, salary, departments_id)
-VALUES (1, 'singer', 25000, 1),
-(2, 'musician', 30000, 3);
+VALUES (1, 'Library Director', 100000, 1),
+(2, 'Head of Technical Services', 90000, 1)
+(3, 'Systems & Metadata Librarian', 80000, 2),
+(4, 'Head of Access Services Librarian', 75000, 3),
+(5, 'Electronic Resources & Serials Librarian', 75000, 2),
+(6, 'Reference Librarian', 75000, 3),
+(7, 'Library Clerk', 40000, 3);
+
+
 
