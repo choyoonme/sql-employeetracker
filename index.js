0ccb64b2ebadc6cd8dcdc400b81ffb54857d92d8
choyoonme <yoonme@gmail.com>
2022-06-01 18:11:32 -0400

    apply document formatter

--- index.js
+++ index.js
@@ -1,81 +1,59 @@
-//Dependencies
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-require('console.table');
-const connection = mysql
-    .createConnection(
-        {
-            host: 'localhost',
-            user: 'root',
-            database: 'employee_db'
-        });
+// Dependencies
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+require("console.table");
+const connection = mysql.createConnection({host: "localhost", user: "root", database: "employee_db"});
 
-//connect and call prompt function
-connection
-    .connect(
-        function (err, results) {
-            if (err) throw err
-            prompt();
-        });
+// connect and call prompt function
+connection.connect(function (err, results) {
+    if (err) 
+        throw err;
+    
 
-//main function
+    prompt();
+});
+
+// main function
 async function prompt() {
-    const answers = await inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'choice',
-                message: 'Choose action:',
-                choices: [
-                    'View All Employees',
-                    'View By Department',
-                    'View By Role',
-                    'Add New Employee',
-                    'Add Role',
-                    'Add Department',
-                    'Update Employee',
-                    'Exit'
-                ]
-            }
-        ])
-        .then(answers => {
-            switch (answers
-                .choice) {
-                case 'View All Employees':
-                    viewAllEmployees();
-                    break;
-                case 'View By Department':
-                    viewByDepartment();
-                    break;
-                case 'View By Role':
-                    viewByRole();
-                    break;
-                case 'Add New Employee':
-                    addEmployee();
-                    break;
-                case 'Add Role':
-                    addRole();
-                    break;
-                case 'Add Department':
-                    addDepartment();
-                    break;
-                case 'Update Employee':
-                    updateEmployee();
-                    break;
-                case 'EXIT':
-                    connection
-                        .end();
-                    break;
-
-            }
-        })
-};
+    const answers = await inquirer.prompt([{
+            type: "list",
+            name: "choice",
+            message: "Choose action:",
+            choices: [
+                "View All Employees",
+                "View By Department",
+                "View By Role",
+                "Add New Employee",
+                "Add Role",
+                "Add Department",
+                "Update Employee",
+                "Exit",
+            ]
+        },]).then((answers) => {
+        switch (answers.choice) {
+            case "View All Employees": viewAllEmployees();
+                break;
+            case "View By Department": viewByDepartment();
+                break;
+            case "View By Role": viewByRole();
+                break;
+            case "Add New Employee": addEmployee();
+                break;
+            case "Add Role": addRole();
+                break;
+            case "Add Department": addDepartment();
+                break;
+            case "Update Employee": updateEmployee();
+                break;
+            case "EXIT": connection.end();
+                break;
+        }
+    });
+}
 
 // view all employees
 function viewAllEmployees() {
-    connection
-        .query(
-            `SELECT employees.id, employees.first_name, employees.last_name, roles.title_name, departments.department_name 
+    connection.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.title_name, departments.department_name 
             AS departments, roles.salary, 
             CONCAT (managers.first_name, " ", managers.last_name) 
             AS manager 
@@ -83,208 +61,255 @@ function viewAllEmployees() {
             LEFT JOIN roles 
             ON employees.roles_id = roles.id
             LEFT JOIN departments ON roles.departments_id = departments.id
-            LEFT JOIN employees managers ON employees.manager_id = managers.id;`,
-            function (err, results) {
-                if (err) throw err
-                console
-                    .table(results)
-                prompt();
-            })
-};
+            LEFT JOIN employees managers ON employees.manager_id = managers.id;`, function (err, results) {
+        if (err) 
+            throw err;
+        
 
-//view by department
+        console.table(results);
+        prompt();
+    });
+}
+
+// view by department
 function viewByDepartment() {
-    connection
-        .query(
-            `SELECT employees.id, employees.first_name, employees.last_name, departments.department_name 
+    connection.query(`SELECT employees.id, employees.first_name, employees.last_name, departments.department_name 
             FROM roles 
             JOIN departments 
             ON departments.id = roles.departments_id 
             JOIN employees 
-            ON employees.roles_id = roles.id;`,
-            function (err, results) {
-                if (err) throw err
-                console
-                    .table(results)
-                prompt();
-            })
-};
+            ON employees.roles_id = roles.id;`, function (err, results) {
+        if (err) 
+            throw err;
+        
 
-//view by role
+        console.table(results);
+        prompt();
+    });
+}
+
+// view by role
 function viewByRole() {
-    connection
-        .query(
-            `SELECT employees.id, employees.first_name, employees.last_name, roles.title_name 
+    connection.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.title_name 
             FROM employees 
             JOIN roles 
-            ON employees.roles_id = roles.id;`,
-            function (err, results) {
-                if (err) throw err
-                console
-                    .table(results)
-                prompt();
-            })
-};
+            ON employees.roles_id = roles.id;`, function (err, results) {
+        if (err) 
+            throw err;
+        
 
-//add new employee
+        console.table(results);
+        prompt();
+    });
+}
+
+// add new employee
 async function addEmployee() {
-    const answers = await inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'firstName',
-                message: 'Enter First Name:'
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "Enter First Name:"
+        }, {
+            type: "input",
+            name: "lastName",
+            message: "Enter Last Name:"
+        }, {
+            type: "list",
+            name: "role",
+            message: "Choose Role:",
+            choices: [
+                {
+                    value: 1,
+                    name: "Library Director"
+                },
+                {
+                    value: 2,
+                    name: "Head of Technical Services"
+                },
+                {
+                    value: 3,
+                    name: "Systems & Metadata Librarian"
+                },
+                {
+                    value: 4,
+                    name: "Head of Access Services"
+                }, {
+                    value: 5,
+                    name: "Electronic Resources & Serials Librarian"
+                }, {
+                    value: 6,
+                    name: "Reference Librarian"
+                }, {
+                    value: 7,
+                    name: "Library Clerk"
+                },
+            ]
+        }, {
+            type: "list",
+            name: "manager",
+            message: "Select Manager:",
+            choices: [
+                {
+                    value: 1,
+                    name: "Library Director"
+                },
+                {
+                    value: 2,
+                    name: "Head of Technical Services"
+                },
+                {
+                    value: 3,
+                    name: "Head of Access Services"
+                },
+                {
+                    value: 4,
+                    name: "Systems & Metadata Librarian"
+                }, {
+                    value: 5,
+                    name: "Electronic Resources & Serials Librarian"
+                }, {
+                    value: 0,
+                    name: "none"
+                },
+            ]
+        },
+    ]);
+    connection.query(`INSERT INTO employees(first_name, last_name, roles_id, manager_id) 
+            VALUES ('${
+        answers.firstName
+    }', '${
+        answers.lastName
+    }' ,'${
+        answers.role
+    }','${
+        answers.manager
+    }');`, function (err, results) {
+        if (err) 
+            throw err;
+        
 
-            },
-            {
-                type: 'input',
-                name: 'lastName',
-                message: 'Enter Last Name:'
-            },
-            {
-                type: 'list',
-                name: 'role',
-                message: 'Choose Role:',
-                choices: [
-                    { value: 1, name: 'Library Director' },
-                    { value: 2, name: 'Head of Technical Services' },
-                    { value: 3, name: 'Systems & Metadata Librarian' },
-                    { value: 4, name: 'Head of Access Services' },
-                    { value: 5, name: 'Electronic Resources & Serials Librarian' },
-                    { value: 6, name: 'Reference Librarian' },
-                    { value: 7, name: 'Library Clerk' }
-                ]
-            },
-            {
-                type: 'list',
-                name: 'manager',
-                message: 'Select Manager:',
-                choices: [
-                    { value: 1, name: 'Library Director' },
-                    { value: 2, name: 'Head of Technical Services' },
-                    { value: 3, name: 'Head of Access Services' },
-                    { value: 4, name: 'Systems & Metadata Librarian' },
-                    { value: 5, name: 'Electronic Resources & Serials Librarian' },
-                    { value: 0, name: 'none' }
-                ]
-            },
+        console.table(results);
+    });
+    connection.end();
+}
 
-        ])
-    connection
-        .query(
-            `INSERT INTO employees(first_name, last_name, roles_id, manager_id) 
-            VALUES ('${answers.firstName}', '${answers.lastName}' ,'${answers.role}','${answers.manager}');`,
-            function (err, results) {
-                if (err) throw err
-                console
-                    .table(results)
-            });
-    connection
-        .end();
-};
-
-
-//add role
+// add role
 async function addRole() {
-    const answers = await inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'titleName',
-                message: 'Enter Name of Title:'
-            },
-            {
-                type: 'input',
-                name: 'salary',
-                message: 'Enter Salary:',
-            },
-            {
-                type: 'list',
-                name: 'departmentID',
-                message: 'Select Department',
-                choices: [
-                    { value: 1, name: 'Administrative' },
-                    { value: 2, name: 'Technical Services' },
-                    { value: 3, name: 'Public Services' },
-                    { value: 4, name: 'Support Staff' }
-                ]
-            }
-        ])
-    connection
-        .query(
-            `INSERT INTO roles(title_name, salary, departments_id) 
-            VALUES ('${answers.titleName}', '${answers.salary}','${answers.departmentID}');`,
-            function (err, results) {
-                if (err) throw err
-                console
-                    .table(results)
-            });
-    connection
-        .end();
-};
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "titleName",
+            message: "Enter Name of Title:"
+        }, {
+            type: "input",
+            name: "salary",
+            message: "Enter Salary:"
+        }, {
+            type: "list",
+            name: "departmentID",
+            message: "Select Department",
+            choices: [
+                {
+                    value: 1,
+                    name: "Administrative"
+                }, {
+                    value: 2,
+                    name: "Technical Services"
+                }, {
+                    value: 3,
+                    name: "Public Services"
+                }, {
+                    value: 4,
+                    name: "Support Staff"
+                },
+            ]
+        },
+    ]);
+    connection.query(`INSERT INTO roles(title_name, salary, departments_id) 
+            VALUES ('${
+        answers.titleName
+    }', '${
+        answers.salary
+    }','${
+        answers.departmentID
+    }');`, function (err, results) {
+        if (err) 
+            throw err;
+        
 
+        console.table(results);
+    });
+    connection.end();
+}
 
-//add department
+// add department
 async function addDepartment() {
-    const answers = await inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'addDept',
-                message: 'Enter Department Name:'
-            }
-        ])
-    connection
-        .query(
-            `INSERT INTO departments (department_name) 
-            VALUES ('${answers.addDept}');`,
-            function (err, results) {
-                if (err) throw err
-                console
-                    .table(results)
-            });
-};
+    const answers = await inquirer.prompt([{
+            type: "input",
+            name: "addDept",
+            message: "Enter Department Name:"
+        },]);
+    connection.query(`INSERT INTO departments (department_name) 
+            VALUES ('${
+        answers.addDept
+    }');`, function (err, results) {
+        if (err) 
+            throw err;
+        
 
-//update employee
+        console.table(results);
+    });
+}
+
+// update employee
 function updateEmployee() {
-    connection
-    .query(
-        'SELECT employees.last_name, roles.title_name FROM employees JOIN roles ON employees.roles_id = roles.id;', function(err, results) {
-         if (err) throw err
-         console.table(results)
-const answers = inquirer
-        .prompt([
-    {
-        type: "rawlist",
-        name: "lastName",
-        message: 'Update Last Name of Employee:',
-        choices: function() 
-        {
-        var lastName = [];
-        for (var i = 0; i < results.length; i++) {
-            lastName.push(results[i].last_name);
-        }
-        return lastName;
-        }
-    },
-    {
-        type:'rawlist',
-        name:'title',
-        message:'Select Title:',
-        choices: function() 
-        {
-        var title = [];
-        for (var i = 0; i < results.length; i ++) {
-            title.push(results[i].title_name);
-        }
-        return title;
-        }
-    },
-    {
-        type:'confirm',
-        name:'end',
-        message:'View update?'
-    }
-    ]); 
+    connection.query("SELECT employees.last_name, roles.title_name FROM employees JOIN roles ON employees.roles_id = roles.id;", function (err, results) {
+        if (err) 
+            throw err;
+        
 
-    })};
+        console.table(results);
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "updateName",
+                message: "Choose Employee to Update:",
+                choices: [
+                    // map array of results
+                ]
+            }, {
+                type: "list",
+                name: "title",
+                message: "Choose New Title:",
+                choices: [
+                    {
+                        value: 1,
+                        name: "Library Director"
+                    },
+                    {
+                        value: 2,
+                        name: "Head of Technical Services"
+                    },
+                    {
+                        value: 3,
+                        name: "Systems & Metadata Librarian"
+                    },
+                    {
+                        value: 4,
+                        name: "Head of Access Services"
+                    }, {
+                        value: 5,
+                        name: "Electronic Resources & Serials Librarian"
+                    }, {
+                        value: 6,
+                        name: "Reference Librarian"
+                    }, {
+                        value: 7,
+                        name: "Library Clerk"
+                    },
+                ]
+            },
+        ]);
+    });
+}
